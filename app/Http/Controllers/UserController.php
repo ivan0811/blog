ad62f2c59e75aa706c5c6f4315aaffb3ecf7ab75
ivan0811 <ivanfaathirza@gmail.com>
2020-12-29 11:10:04 +0700

    initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Kategori;
+
+class UserController extends Controller
+{
+    function categori(){
+        $category = Kategori::all();
+        return view('user.category.category', compact('category'));
+    }
+
+    function createCategory(){
+        return view('user.category.createCategory');
+    }    
+
+    function storeCategory(Request $request){
+        Kategori::create([
+            'nama' => $request->nama_kategori
+        ]);
+        return redirect()->route('category');
+    }
+
+    function EditCategory(Request $request, $id){
+        $kategori = Kategori::findOrFail($id);
+        return view('user.category.editCategory', compact('kategori'));
+    }
+
+    function UpdateCategory(Request $request){
+        $kategori = Kategori::findOrFail($request->id);
+        $kategori->nama = $request->nama_kategori;
+        $kategori->save();
+        return redirect()->route('category');
+    }
+
+    function DeleteCategory($id){
+        Kategori::findOrFail($id)->delete();
+        return redirect()->back();
+    }
+}
